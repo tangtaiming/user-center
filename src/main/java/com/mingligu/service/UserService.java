c78e88ca8f92dd6b0324eb795af59aa08ac992bf
v_tangtaiming <v-tangtaiming@vitasoy-chn.com>
2025-02-28 00:59:13 +0800

    [add] 用户登录功能

--- src/main/java/com/mingligu/service/UserService.java
+++ src/main/java/com/mingligu/service/UserService.java
@@ -3,6 +3,8 @@ package com.mingligu.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.mingligu.model.User;
 
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * 用户服务
 * @author fr_tangtaiming
@@ -20,5 +22,14 @@ public interface UserService extends IService<User> {
      * @return 1 成功， 0 失败
      */
     long userRegister(String userAccount, String userPassword, String checkPassword);
+
+    /**
+     * 用户登录
+     * @param userAccount  用户账号
+     * @param userPassword 用户密码
+     * @param request  http request 请求信息
+     * @return 用户信息
+     */
+    User userLogin(String userAccount, String userPassword, HttpServletRequest request);
 
 }
--- src/main/java/com/mingligu/service/impl/UserServiceImpl.java
+++ src/main/java/com/mingligu/service/impl/UserServiceImpl.java
@@ -1,14 +1,17 @@
 package com.mingligu.service.impl;
+import java.util.Date;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.mingligu.mapper.UserMapper;
 import com.mingligu.model.User;
 import com.mingligu.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
 
+import javax.servlet.http.HttpServletRequest;
 import java.security.MessageDigest;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -20,6 +23,7 @@ import java.util.regex.Pattern;
 * @description 针对表【user(用户表)】的数据库操作Service实现
 * @createDate 2025-02-23 23:40:36
 */
+@Slf4j
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User>
     implements UserService {
@@ -29,6 +33,9 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User>
 
     //盐
     private static final String SALT = "mingligu";
+
+    //用户登录态
+    private static final String USER_LOGIN_STATE = "user_login_state";
 
     public UserServiceImpl(UserMapper userMapper) {
     }
@@ -79,6 +86,55 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User>
         }
 
         return user.getId();
+    }
+
+    @Override
+    public User userLogin(String userAccount, String userPassword, HttpServletRequest request) {
+        //1、校验
+        if (StringUtils.isAnyBlank(userAccount, userPassword)) {
+            // todo 修改为自定义异常
+            return null;
+        }
+        if (userAccount.length() < 4) {
+            // todo 修改为自定义异常
+            return null;
+        }
+        if (userPassword.length() < 8) {
+            // todo 修改为自定义异常
+            return null;
+        }
+        //账户校验特殊字符
+        if (validateExitUserAccount(userAccount)) {
+            // todo 修改为自定义异常
+            return null;
+        }
+
+        //2、加密
+        String newPassword = DigestUtils.md5DigestAsHex((SALT + userPassword).getBytes());
+        QueryWrapper<User> wh = new QueryWrapper<>();
+        wh.eq("userAccount", userAccount);
+        wh.eq("userPassword", newPassword);
+        User user = this.baseMapper.selectOne(wh);
+        //用户不存在
+        if (null == user) {
+            log.info("user login failed, userAccount cannot match userPassword");
+            // todo 修改为自定义异常
+            return null;
+        }
+
+        //3、用户信息脱敏
+        User clearUser = new User();
+        clearUser.setUserAccount(user.getUserAccount());
+        clearUser.setUserName(user.getUserName());
+        clearUser.setAvatar(user.getAvatar());
+        clearUser.setGender(user.getGender());
+        clearUser.setPhone(user.getPhone());
+        clearUser.setEmail(user.getEmail());
+        clearUser.setUserStatus(user.getUserStatus());
+        //4、session中存储用户信息
+        request.getSession().setAttribute(USER_LOGIN_STATE, clearUser);
+
+        return user;
     }
 
     /**
